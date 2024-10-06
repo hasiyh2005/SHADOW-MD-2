4f40f439c3e9dc69888e61bae5b00eee5df85680
hasiyh2005 <150225150+hasiyh2005@users.noreply.github.com>
2024-10-06 14:29:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "OB0DDaQL#vuaE-PYnNKjqKlzJW5K7kvhCEuD3-hkJ6BanXBVbCuk",
+SESSION_ID: process.env.SESSION_ID || "AdBCQZjC#hTtUJsLi_yU-9_EMWj4nixbh_1VzqJaD1Scmqm_k514",
 SUDO_NB: process.env.SUDO_NB || "94767910958",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_BIO: process.env.AUTO_BIO || "false",
